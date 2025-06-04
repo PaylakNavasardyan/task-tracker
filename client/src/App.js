0b3a14668edf66a1fd2dbe227657e96a8282e3d7
Paylak Navasardyan <paylak2002n@gmail.com>
2025-06-04 23:18:18 +0400

    New folder: client and design for login page

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+import Login from './Login/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Login />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Login/Login.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import classes from './Login.module.css';
+import { Link } from 'react-router-dom';
+
+export default function Login() {
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [remember, setRemember] = useState(false);
+
+    const handleClik = () => {
+      setRemember(!remember)
+    };
+
+  return (
+    <div className={classes.login}>
+      <div className={classes.loginBody}>
+        <div className={classes.loginBodyTitle}>
+            <h1>Task Tracker</h1>
+        </div>
+        <div className={classes.loginBodyFields}>
+            <h2>Login</h2>
+            <input 
+                className={classes.input}
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+            />
+            <input 
+                className={classes.loginBodyFields}
+                type='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+            />
+            <div className={classes.loginBodyFieldsDown}>
+              <div className={classes.loginBodyFieldsDownRemember}>
+                <input
+                  className={classes.loginBodyRadio} 
+                  type='checkbox'
+                  checked={remember}
+                  onChange={handleClik}
+                />
+                <span>Remember me</span>  
+              </div>
+                <Link
+                  className={classes.loginBodyLink}
+                  to='Forgot'
+                >Forgot password</Link>
+            </div>
+            <div className={classes.loginBodyButton}> 
+              <button>Login</button>
+            </div>
+        </div>
+      </div>
+      <div className={classes.loginBackToRagistration}>
+        <p>If you haven't registered yet, click here</p>
+        <Link
+          className={classes.loginBackToRagistrationLink}
+          to='Registration'
+        >Registration</Link>
+      </div>
+    </div>
+  )
+}
