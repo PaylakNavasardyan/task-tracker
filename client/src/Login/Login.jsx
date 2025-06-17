4a6043a17e5a602af3a76d0b7027a504a8291121
Paylak Navasardyan <paylak2002n@gmail.com>
2025-06-17 14:57:36 +0400

    Sending initial data to the server

--- client/src/Login/Login.jsx
+++ client/src/Login/Login.jsx
@@ -1,6 +1,7 @@
 import { useState, useRef } from 'react';
 import classes from './Login.module.css';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Login() {
     const [name, setName] = useState('');
@@ -21,6 +22,25 @@ export default function Login() {
       setRemember(!remember)
     };
 
+    const payload = {
+      name,
+      password,
+      remember
+    }
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+
+      try {
+        document.cookie = `name=${name}; path=/`;
+        document.cookie = `remember me=${remember}; path=/`;
+
+        await axios.post('http://localhost:5000/Login', payload)
+      } catch(error) {
+        console.error('error', error)
+      }
+    }
+    
   return (
     <div className={classes.login}>
       <div className={classes.loginBody}>
@@ -29,44 +49,48 @@ export default function Login() {
         </div>
         <div className={classes.loginBodyFields}>
             <h2>Login</h2>
-            <input 
-                className={classes.input}
-                type="text"
-                placeholder='Username'
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                onKeyDown={(e) => handleKeyDown(e, passwordInputRef)}
-                ref={nameInputRef}
-                required
-            />
-            <input 
-                className={classes.loginBodyFields}
-                type='password'
-                placeholder='Password'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                onKeyDown={(e) => handleKeyDown(e, undefined)}
-                ref={passwordInputRef}
-                required
-            />
-            <div className={classes.loginBodyFieldsDown}>
-              <div className={classes.loginBodyFieldsDownRemember}>
-                <input
-                  className={classes.loginBodyRadio} 
-                  type='checkbox'
-                  checked={remember}
-                  onChange={handleClik}
-                />
-                <span>Remember me</span>  
+            <form onSubmit={handleSubmit}>
+              <input 
+                  className={classes.input}
+                  type="text"
+                  placeholder='Username'
+                  name='name'
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  onKeyDown={(e) => handleKeyDown(e, passwordInputRef)}
+                  ref={nameInputRef}
+                  required
+              />
+              <input 
+                  className={classes.loginBodyFields}
+                  type='password'
+                  placeholder='Password'
+                  name='password'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  onKeyDown={(e) => handleKeyDown(e, undefined)}
+                  ref={passwordInputRef}
+                  required
+              />
+              <div className={classes.loginBodyFieldsDown}>
+                <div className={classes.loginBodyFieldsDownRemember}>
+                  <input
+                    className={classes.loginBodyRadio} 
+                    type='checkbox'
+                    checked={remember}
+                    onChange={handleClik}
+                  />
+                  <span>Remember me</span>  
+                </div>
+                  <Link
+                    className={classes.loginBodyLink}
+                    to='/Forgot'
+                  >Forgot password</Link>
               </div>
-                <Link
-                  className={classes.loginBodyLink}
-                  to='/Forgot'
-                >Forgot password</Link>
-            </div>
-            <div className={classes.loginBodyButton}> 
-              <button>Login</button>
-            </div>
+              <div className={classes.loginBodyButton}> 
+                <button>Login</button>
+              </div>
+            </form>
         </div>
       </div>
       <div className={classes.loginBackToRagistration}>
