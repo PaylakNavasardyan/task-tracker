07a9bb19e08bdb1bd12e5c84cd61ef51f576456a
Paylak Navasardyan <paylak2002n@gmail.com>
2025-07-11 16:59:05 +0400

    Feat: Add confirmation dialog before user logout

--- client/src/Tasks/TasksHeader/TasksHeader.jsx
+++ client/src/Tasks/TasksHeader/TasksHeader.jsx
@@ -6,13 +6,19 @@ import { MdDriveFileRenameOutline, MdDelete  } from "react-icons/md";
 import { useNavigate } from 'react-router-dom';
 
 export default function TasksHeader() {
-    const [show, setShow] = useState(false);
-
-    const navigate = useNavigate();
-    
+    // Display settings menu
+    const [show, setShow] = useState(false);    
     const handleClick = () => {
         setShow((show) => !show)
     };
+    
+    // Display log out confirmation panel
+    const [panel, setPanel] = useState(false);
+    const handleConfirm = () => {
+        setPanel((panel) => !panel)
+    };
+
+    const navigate = useNavigate();
     
     function getCookie(name) {
         const cookieName = name + '=';
@@ -33,14 +39,34 @@ export default function TasksHeader() {
     const userEmail = getCookie('email');
     console.log('User Email from Cookie:', userEmail);
 
-    const onLogOutClikc = () => {
+    const onLogOutClikcYes = () => {
         localStorage.removeItem('loggedIn');
         localStorage.removeItem('remember');
+
         navigate('/Login')
+    };
+
+    const onLogOutClickNo = () => {
+        handleConfirm()
     };
 
   return (
     <div className={classes.header}>  
+        <div className={`${panel ? classes.panelIsOpen : classes.hiddenPanel}`}>
+            <span>Do you really want to log out?</span>
+
+            <button 
+                className={`${classes.btn1} ${classes.btn}`}
+                onClick={onLogOutClikcYes}
+                >Yes
+            </button>
+            <button 
+                className={`${classes.btn2} ${classes.btn}`}
+                onClick={onLogOutClickNo}
+                >No
+            </button>
+        </div>
+
         <h2>Task Tracker</h2>
 
         <div className={classes.headerBodySettingIcon}>
@@ -68,7 +94,7 @@ export default function TasksHeader() {
                     <IoIosLogOut 
                         size={'20px'}
                     />
-                    <p  onClick={onLogOutClikc}>Log Out</p>
+                    <p  onClick={handleConfirm}>Log Out</p>
                 </div>
                 <div className={classes.settingSmallIcon}>
                     <MdDelete 
